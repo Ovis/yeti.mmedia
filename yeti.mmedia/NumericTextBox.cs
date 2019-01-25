ee43fc4ac96b08cc232936e9be02d289076f3a38
Ovis <me+github@hitsujin.jp>
2019-01-25 21:22:21 +0900

    インデント等修正

--- yeti.mmedia/NumericTextBox.cs
+++ yeti.mmedia/NumericTextBox.cs
@@ -17,69 +17,63 @@ using System.Globalization;
 
 namespace Yeti.Controls
 {
-	/// <summary>
-	/// Define a TextBox that allow only integer numbers.
-	/// </summary>
-	public class NumericTextBox : TextBox
-	{
-		public NumericTextBox()
-      :base()
-		{
-		}
-
-    protected override void OnKeyPress(KeyPressEventArgs e)
+    /// <summary>
+    /// Define a TextBox that allow only integer numbers.
+    /// </summary>
+    public class NumericTextBox : TextBox
     {
-      base.OnKeyPress(e);
-      if ( (!e.Handled) && ( "1234567890\b".IndexOf(e.KeyChar) < 0) )
-      {
-        Yeti.Sys.Win32.MessageBeep(Yeti.Sys.BeepType.SimpleBeep);
-        e.Handled = true;
-      }
-    }
+        public NumericTextBox()
+      : base()
+        {
+        }
 
-	  public event EventHandler FormatError;
-    public event EventHandler FormatValid;
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
+            if ((!e.Handled) && ("1234567890\b".IndexOf(e.KeyChar) < 0))
+            {
+                Sys.Win32.MessageBeep(Yeti.Sys.BeepType.SimpleBeep);
+                e.Handled = true;
+            }
+        }
 
-    protected virtual void OnFormatError(EventArgs e)
-    {
-      if (FormatError != null)
-      {
-        FormatError(this, e);
-      }
-    }
+        public event EventHandler FormatError;
+        public event EventHandler FormatValid;
 
-    protected virtual void OnFormatValid(EventArgs e)
-    {
-      if (FormatValid != null)
-      {
-        FormatValid(this, e);
-      }
-    }
+        protected virtual void OnFormatError(EventArgs e)
+        {
+            FormatError?.Invoke(this, e);
+        }
 
-    protected override void OnTextChanged(EventArgs e)
-    {
-      try
-      {
-        int.Parse(this.Text, NumberStyles.Integer);
-        OnFormatValid(e);
-      }
-      catch
-      {
-        OnFormatError(e);
-      }
-      base.OnTextChanged (e);
-    }
+        protected virtual void OnFormatValid(EventArgs e)
+        {
+            FormatValid?.Invoke(this, e);
+        }
 
-    public int Value
-    {
-      get
-      {
-        return int.Parse(this.Text, NumberStyles.Integer);
-      }
-      set
-      {
-        this.Text = value.ToString();
-      }
+        protected override void OnTextChanged(EventArgs e)
+        {
+            try
+            {
+                int.Parse(Text, NumberStyles.Integer);
+                OnFormatValid(e);
+            }
+            catch
+            {
+                OnFormatError(e);
+            }
+            base.OnTextChanged(e);
+        }
+
+        public int Value
+        {
+            get
+            {
+                return int.Parse(Text, NumberStyles.Integer);
+            }
+            set
+            {
+                Text = value.ToString();
+            }
+        }
     }
-  }
 }
